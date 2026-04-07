e71b40069c5a33c5efcbcad776408d7b4758a4dc
zishan <zishanza436@gmail.com>
2026-04-07 12:56:46 +0530

    endpoint updated

--- frontend/digital-persona/src/api.js
+++ frontend/digital-persona/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://ec2-3-110-143-248.ap-south-1.compute.amazonaws.com";
 
 export async function fetchLinkedInProfile(userID) {
   const res = await fetch(`${BASE_URL}/api/v1/requestLDdata`, {
